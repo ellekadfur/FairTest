bc698fb36dff5acc3aa56272e789fb2ff6c902bb
Lamar J. Williams IV <lamarjwilliamsiv@gmail.com>
2018-03-20 20:56:25 -0700

    Init Project

---
+++ FairTest/FairTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FairTest
+//
+//  Created by Lamar Jay Caaddfiir on 3/20/18.
+//  Copyright © 2018 Lamar Jay Caaddfiir All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
